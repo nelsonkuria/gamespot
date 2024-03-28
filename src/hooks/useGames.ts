ed022b6ef4edc23f568ca32049f9b1b5f2dc3d04
Nelson <nelson.kuria@smile.io>
2024-03-28 07:50:09 +0300

    displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,15 @@
 import { useEffect, useState } from 'react'
 
+export interface Platform {
+  id: number
+  name: string
+  slug: string
+}
 export interface Game {
   id: number
   name: string
   background_image: string
+  parent_platforms: { platform: Platform }[]
 }
 
 interface FetchGamesResponse {

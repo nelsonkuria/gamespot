534bb9f6b83bfb64e3f8f8f8da965f5dfbcfcd27
Nelson <nelson.kuria@smile.io>
2024-03-28 07:05:42 +0300

    create custom hook to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,38 +1,8 @@
 import { Text } from '@chakra-ui/react'
-import { useEffect, useState } from 'react'
-
-interface Game {
-  id: number
-  name: string
-}
-
-interface FetchGamesResponse {
-  count: number
-  results: Game[]
-}
+import useGames from '../hooks/useGames'
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([])
-  const [error, setError] = useState('')
-
-  useEffect(() => {
-    fetch(
-      'https://api.rawg.io/api/gasmes?' +
-        new URLSearchParams({ key: 'fde7be0582c74645b254ba5c0c2587a4' }),
-    )
-      .then((res) => {
-        if (res.ok) return res.json()
-
-        switch (res.status) {
-          case 404:
-            throw new Error('Not found! Check your url.')
-          default:
-            throw new Error('Something bad happened!')
-        }
-      })
-      .then((data: FetchGamesResponse) => setGames(data.results))
-      .catch((err: Error) => setError(err.message))
-  }, [])
+  const { games, error } = useGames()
 
   return (
     <>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+
+interface Game {
+  id: number
+  name: string
+}
+
+interface FetchGamesResponse {
+  count: number
+  results: Game[]
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([])
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    const controller = new AbortController()
+
+    fetch(
+      'https://api.rawg.io/api/games?' +
+        new URLSearchParams({ key: 'fde7be0582c74645b254ba5c0c2587a4' }),
+      { signal: controller.signal },
+    )
+      .then((res) => {
+        if (res.ok) return res.json()
+
+        switch (res.status) {
+          case 404:
+            throw new Error('Not found! Check your url.')
+          default:
+            throw new Error('Something bad happened!')
+        }
+      })
+      .then((data: FetchGamesResponse) => setGames(data.results))
+      .catch((err) => {
+        if (err.name === 'AbortError') return
+        setError(err.message)
+      })
+
+    return () => controller.abort()
+  }, [])
+
+  return { games, error }
+}
+
+export default useGames

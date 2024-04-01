3de06d24a6b5a8703e3a214240b2b4abdf80d72d
Nelson <nelson.kuria@smile.io>
2024-04-01 06:09:25 +0300

    filter games by genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -5,7 +5,9 @@ interface FetchResponse<T> {
   results: T[]
 }
 
-const useData = <T>(endpoint: string) => {
+// type RequestConfig = { [key: string]: string | number | undefined }
+
+const useData = <T>(endpoint: string, requestConfig?: string) => {
   const [data, setData] = useState<T[]>([])
   const [error, setError] = useState('')
   const [isLoading, setIsLoading] = useState(false)
@@ -16,7 +18,10 @@ const useData = <T>(endpoint: string) => {
     setIsLoading(true)
     fetch(
       `https://api.rawg.io/api${endpoint}?` +
-        new URLSearchParams({ key: 'fde7be0582c74645b254ba5c0c2587a4' }),
+        new URLSearchParams({
+          key: 'fde7be0582c74645b254ba5c0c2587a4',
+          ...(requestConfig ? JSON.parse(requestConfig) : {}),
+        }),
       { signal: controller.signal },
     )
       .then((res) => {
@@ -40,7 +45,7 @@ const useData = <T>(endpoint: string) => {
       })
 
     return () => controller.abort()
-  }, [endpoint])
+  }, [endpoint, requestConfig])
 
   return { data, error, isLoading }
 }

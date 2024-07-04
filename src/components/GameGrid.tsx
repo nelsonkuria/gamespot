3be82e9178b13ef0c437350ca5726643d5cc46c2
nkuriam <nelsonkuriam@gmail.com>
2024-07-04 04:33:17 +0300

    refactor use games with react query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,10 +10,10 @@ interface Props {
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading } = useGames(gameQuery)
+  const { data, error, isPending } = useGames(gameQuery)
   const skeletons = [1, 2, 3, 4, 5, 6]
 
-  if (error) return <Text>{error}</Text>
+  if (error) return <Text>{error.message}</Text>
 
   return (
     <SimpleGrid
@@ -26,13 +26,13 @@ const GameGrid = ({ gameQuery }: Props) => {
       spacing={6}
       padding="10px"
     >
-      {isLoading &&
+      {isPending &&
         skeletons.map((skeleton) => (
           <GameCardContainer key={skeleton}>
             <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      {data.map((game) => (
+      {data?.results.map((game) => (
         <GameCardContainer key={game.id}>
           <GameCard game={game} />
         </GameCardContainer>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
+import { useQuery } from '@tanstack/react-query'
 import { GameQuery } from '../App'
-import useData from './useData'
+import { FetchResponse } from './useData'
 
 export interface Platform {
   id: number
@@ -15,15 +16,59 @@ export interface Game {
   rating_top: number
 }
 
-const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    '/games',
-    JSON.stringify({
-      genres: gameQuery.genre?.id,
-      parent_platforms: gameQuery.platform?.id,
-      ordering: gameQuery.sortOrder,
-      search: gameQuery.searchText,
-    }),
+// const useGames = (gameQuery: GameQuery) =>
+//   useData<Game>(
+//     '/games',
+//     JSON.stringify({
+//       genres: gameQuery.genre?.id,
+//       parent_platforms: gameQuery.platform?.id,
+//       ordering: gameQuery.sortOrder,
+//       search: gameQuery.searchText,
+//     }),
+//   )
+
+function formatQuery(query: GameQuery) {
+  const params: { [key: string]: string } = {
+    genres: query.genre?.id ? `${query.genre?.id}` : '',
+    parent_platforms: query.platform?.id ? `${query.platform?.id}` : '',
+    ordering: query.sortOrder,
+    search: query.searchText,
+  }
+
+  return Object.fromEntries(
+    Object.entries(params).filter(([, value]) => Boolean(value)),
   )
+}
+
+async function fetchGames(query: GameQuery | null) {
+  const response = await fetch(
+    'https://api.rawg.io/api/games?' +
+      new URLSearchParams({
+        key: 'fde7be0582c74645b254ba5c0c2587a4',
+        ...(query ? formatQuery(query) : {}),
+      }),
+  )
+
+  if (!response.ok) {
+    switch (response.status) {
+      case 404:
+        throw new Error('404 not found. Please check your url.')
+      default:
+        throw new Error(`Request failed with a status of ${response.status}`)
+    }
+  }
+
+  return response.json()
+}
+
+const useGames = (gameQuery: GameQuery) => {
+  const query = Object.keys(gameQuery).length ? gameQuery : null
+
+  return useQuery<FetchResponse<Game>, Error>({
+    queryKey: query ? ['games', { ...formatQuery(gameQuery) }] : ['games'],
+    queryFn: () => fetchGames(query),
+    staleTime: 60 * 1000,
+  })
+}
 
 export default useGames
